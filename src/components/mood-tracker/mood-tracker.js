0de9aa2349cef384cb048a8938392b80c651e615
Vishwa Mehta <32997409+mehtavishwa30@users.noreply.github.com>
2020-03-21 15:44:38 +0530

    Update mood-tracker.js

--- src/components/mood-tracker/mood-tracker.js
+++ src/components/mood-tracker/mood-tracker.js
@@ -22,22 +22,26 @@ function MoodTracker() {
         <div id="moodTracker">
           <form onSubmit={submitFn} name="moodForm">
             <div>
-              <div className="label fit">
+              <label className="label fit">
+                <span className="visuallyhidden">Enter your name</span>
                 <input type="text" placeholder="Enter your name" name="Name" required />
-              </div>
-              <div className="label fit">
+              </label>
+              <label className="label fit">
+                <span className="visuallyhidden">How do you feel?</span>
                 <input type="text" placeholder="How do you feel?" name="desc" required />
-              </div>
+              </label>
             </div>
             <div>
               <div className="inputGroup left">
                 <button type="button">
+                  <span className="visuallyhidden">Select a previous date</span>
                   <i className="fa fa-caret-left"></i>
                 </button>
-                <div className="label">
+                <label className="label">
                   <input type="text" placeholder="21.03.20" name="date" />
-                </div>
+                </label>
                 <button type="button">
+                  <span className="visuallyhidden">Select a later date</span>
                   <i className="fa fa-caret-right"></i>
                 </button>
               </div>
@@ -47,7 +51,8 @@ function MoodTracker() {
                 </button>
               </div>
               <div className="select right">
-                <div className="label">
+                <label className="label">
+                <span className="visuallyhidden">Select a mood type</span>
                   <select name="moodType" required>
                     <option>Select a mood type</option>
                     <option>Happy</option>
@@ -60,7 +65,7 @@ function MoodTracker() {
                     <option>Blah</option>
                     <option>Not sure</option>
                   </select>
-                </div>
+                </label>
               </div>
               <div className="button-wrap right">
                 <button className="submit">
